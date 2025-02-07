5385ae13a8bce314878e785548ba07523908019e
sanotogii <nasaereh@gmail.com>
2025-02-07 15:31:47 +0100

    Refactor main entry point to streamline application startup

--- transcriber_app.py
+++ transcriber_app.py
@@ -1,9 +1,5 @@
 from ui_components import TranscriberUI
-from transcription_manager import TranscriptionManager
-
-def main():
-    app = TranscriberUI()
-    app.run()
 
 if __name__ == "__main__":
-    main()
+    app = TranscriberUI()
+    app.run()
