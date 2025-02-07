514de94d424661c866c4985af5857af540cbadf4
sanotogii <nasaereh@gmail.com>
2025-02-07 15:22:44 +0100

    Add main entry point for the Transcriber application

---
+++ transcriber_app.py
@@ -0,0 +1,9 @@
+from ui_components import TranscriberUI
+from transcription_manager import TranscriptionManager
+
+def main():
+    app = TranscriberUI()
+    app.run()
+
+if __name__ == "__main__":
+    main()
